b6f28984b3b1dc16c576369ae3952141653ce930
Fábio Cordeiro <fabio@pucminas.br>
2023-05-10 21:34:31 -0300

    limpando a main

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Aplicação BASE!");
 Console.WriteLine("Atividade 5!");
 
 Questao5.Executar();
--- Questao1.cs
+++ Questao1.cs
@@ -1,5 +1,26 @@
-public class Questao1{
+class Questao1
+{   
     public static void Executar(){
-        Console.WriteLine("Resolução da Questão 1");
+        //Declarando Vetor de Inteiros de tamanho 20
+        int[]N = new int[20];
+
+        for (int i = 0; i < 20; i++){
+            Console.WriteLine($"Entre com valor para N[{i}]");
+            N[i] = int.Parse(Console.ReadLine());
+        }
+
+        int menor = N[0];
+        int posicao = 0;
+
+        for (int i = 0; i < 20; i++)
+        {
+            if (N[i] < menor ){
+                menor = N[i];
+                posicao = i;
+            }
+        }
+
+        Console.WriteLine($"O menor elemento do Vetor N é {menor} e foi encontrado na posição {posicao}");
     }
+    
 }
